f792be826241d5b068cd7915243c42bfd1dd8d2e
Alex Kubiesa <alex.kubiesa@outlook.com>
2021-05-20 16:54:36 +0100

    Solve Problem 71 in C#.

---
+++ Problem_71/Problem_71/FareySequence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+using Rationals;
+
+namespace Problem_71
+{
+    public static class FareySequence
+    {
+        public static Rational GetPreviousTerm(BigInteger sequenceIndex, Rational term)
+        {
+            if (sequenceIndex < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sequenceIndex), "Farey sequence index must be positive.");
+            }
+
+            term = term.CanonicalForm;
+
+            if (term.Denominator > sequenceIndex)
+            {
+                throw new ArgumentException("The term does not appear in the specified Farey sequence.");
+            }
+
+            var previousTerm = Rational.Zero;
+            var mediant = Mediant(previousTerm, term).CanonicalForm;
+
+            while (mediant.Denominator <= sequenceIndex)
+            {
+                previousTerm = mediant;
+                mediant = Mediant(previousTerm, term).CanonicalForm;
+            }
+
+            return previousTerm;
+        }
+
+        public static Rational Mediant(Rational x, Rational y)
+        {
+            return (Rational) (x.Numerator + y.Numerator) / (x.Denominator + y.Denominator);
+        }
+    }
+}
---
+++ Problem_71/Problem_71/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Rationals;
+
+namespace Problem_71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int fareySequenceIndex = 1_000_000;
+            var term = (Rational) 3 / 7;
+            var previousTerm = FareySequence.GetPreviousTerm(fareySequenceIndex, term);
+            var solution = previousTerm.Numerator;
+
+            Console.WriteLine($"Solution: {solution}");
+            Console.ReadLine();
+        }
+    }
+}
