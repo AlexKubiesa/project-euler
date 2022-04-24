727ade1826bdf8abe230766fc2c07414885c4a2a
Alex Kubiesa <alex.kubiesa@outlook.com>
2022-04-24 21:49:33 +0100

    Use Euler phi function for Problem 754.

--- Problem_754/Problem_754/Program.cs
+++ Problem_754/Problem_754/Program.cs
@@ -1,7 +1,23 @@
-﻿
-using System.Globalization;
+﻿using System.Globalization;
 
 const long MODULUS = 1_000_000_007;
+
+// Analysis
+// ========
+//
+// G(n) is the product of all the g(i) with i <= n, and g(i) is the product of all j < i coprime to i.
+// This means that the same number j appears multiple times for different i. The number of times j
+// appears in the product G(n) is equal to the number of integers m coprime to j with j + 1 <= m <= n.
+//
+// Let f_j(n) be the number of integers m coprime to j with 1 <= m <= n. Then f_j(j) = phi(j) (the
+// Euler totient function), and similarly the number of integers m coprime to j for j + 1 <= m <= 2j
+// is phi(j), and similarly for 2j + 1 <= m <= 3j, etc. So if n = qj + r with 0 <= r < j, then
+// f_j(n) = q * f_j(j) + f_j(r) = q * phi(j) + f_j(r). The exponent j should be raised to is
+// f_j(n) - f_j(j) = (q - 1) * phi(j) + f_j(r).
+//
+// Call f_j(r) the "residual totient" of j with respect to n.
+
+// TODO: Try generating all pairs of coprime integers using the tree algorithm.
 
 while (true)
 {
@@ -23,6 +39,25 @@ while (true)
 }
 
 static long ProductOfGaussFactorials(int n)
+{
+    long res = 1;
+
+    bool[] isPrime = PrimeSieve(n);
+
+    for (long m = 1; m <= n; m++)
+    {
+        List<long> primeFactors = PrimeFactors(m, isPrime);
+        bool[] isCoprimeToM = CoprimeSieve(m, primeFactors);
+        long exponent = ContributionExponent(m, n, isCoprimeToM);
+        long mContribution = Exp(m, exponent);
+        res *= mContribution;
+        res %= MODULUS;
+    }
+
+    return res;
+}
+
+static long ProductOfGaussFactorials_UsingGaussFactorial(int n)
 {
     long res = 1;
 
@@ -115,4 +150,44 @@ static long HCF(long a, long b)
         (a, b) = (b, a % b);
 
     return a;
+}
+
+/// <summary>
+/// Calculates the exponent to which to raise m in the product G(n) of Gauss factorials. Requires a sieve for integers coprime to m.
+/// </summary>
+static long ContributionExponent(long m, long n, bool[] isCoprimeToM)
+{
+    (long q, long r) = Math.DivRem(n, m);
+
+    long residualTotient = 0;
+    for (long i = 1; i <= r; i++)
+        if (isCoprimeToM[i])
+            residualTotient++;
+
+    long totient = residualTotient;
+    for (long i = r + 1; i < m; i++)
+        if (isCoprimeToM[i])
+            totient++;
+
+    return (q - 1) * totient + residualTotient;
+}
+
+static long Exp(long n, long k)
+{
+    long res = 1;
+
+    while (k != 0)
+    {
+        if ((k & 1) == 1)
+        {
+            res *= n;
+            res %= MODULUS;
+        }
+
+        k >>= 1;
+        n *= n;
+        n %= MODULUS;
+    }
+
+    return res;
 }
